692c70d88ce36b285b568c697ea56df203f15354
mark3-sasc <mark-sasc@dataindependence.net>
2021-02-24 18:36:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mark-chess
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mark-chess)
---
+++ index.ts
@@ -0,0 +1,6 @@
+import "./style.css";
+import "./extra.css";
+import { basicVar } from "./basic";
+import { extraVar } from "./extra";
+
+console.log(basicVar + extraVar); 
